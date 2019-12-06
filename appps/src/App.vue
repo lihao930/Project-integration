e7a7c3b2e08b6c6451c8091a33763220ee6197a0
lihao930 <1840911465@qq.com>
2019-12-06 11:18:38 +0800

    六日考技

---
+++ appps/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <div class="main">
+      <router-view />
+    </div>
+    <div class="nav">
+      <router-link to="/">首页</router-link>|
+      <router-link to="/about">我的</router-link>
+    </div>
+  </div>
+</template>
+
+<style>
+.app{
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+.main{
+  flex: 1
+}
+.nav{
+  width: 100%;
+  height: 50px;
+}
+</style>
---
+++ appps/src/views/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="home">
+    <div class="swp">
+      <swiper :options="swiperOption">
+        <swiper-slide>1</swiper-slide>
+        <swiper-slide>2</swiper-slide>
+        <swiper-slide>3</swiper-slide>
+      <div class="swiper-pagination"></div>
+      </swiper>
+    </div>
+    <div class="bod">
+      <dl v-for="(item,index) in list" :key="index">
+        <dt>1</dt>
+        <dd>{{item.nm}}</dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data(){
+    return{
+      list:[
+        {
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        },{
+          nm:'成单奖励'
+        }
+      ],
+      swiperOption:{
+        pagination:{
+          el:'.swiper-pagination'
+        },
+        loop:true,
+        autoplay:true
+      }
+    }
+  },
+  components: {
+  }
+}
+</script>
+<style scoped>
+.swp{
+  width: 100%;
+  height: 100px;
+  overflow: hidden;
+  background: #ccc;
+  text-align: center;
+  line-height: 100px;
+}
+.bod{
+  width: 100%;
+  height: 160px;
+  margin: 0;
+  padding: 0;
+}
+dl{
+  width: 25%;
+  text-align: center;
+  display: inline-block;
+}
+dt{
+  display: inline-block;
+}
+dd{
+  margin: 0;
+  padding: 0;
+}
+</style>
